0f09b184b201c8b8b501221ab80a03c8e1740a35
Victor <vctr@pop-os.localdomain>
2023-01-12 18:32:21 -0300

    adding prism in api

---
+++ prisma/migrations/20230112204940_specification/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "specification" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "descriptin" TEXT NOT NULL,
+
+    CONSTRAINT "specification_pkey" PRIMARY KEY ("id")
+);
