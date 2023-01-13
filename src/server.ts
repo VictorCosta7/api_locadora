e271e549c55cd1a89588a72aa05349ba3bcd6605
VictorCosta7 <engvictorcosta10@gmail.com>
2023-01-13 10:30:22 -0300

    changes

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,14 @@
-<<<<<<< HEAD
 import express, { json } from "express";
 import { categoriesRoutes } from "./routes/categories.routes";
 import { specificationRoutes } from "./routes/specifications.routes";
-=======
-import "reflect-metadata";
-import express from "express";
-import swaggerUi from "swagger-ui-express";
-import swaggerFile from "./swagger.json";
-import { router } from "./routes";
->>>>>>> 7437eba (adding prism in api)
 
 const app = express();
 
 app.use(express.json());
 
-<<<<<<< HEAD
 app.use("/categories", categoriesRoutes);
 
 app.use("/specifications", specificationRoutes);
-=======
-app.use(router);
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
->>>>>>> 7437eba (adding prism in api)
 
 app.listen(3333, () => {
   console.log("Server is running!");
